b26c3dc735d9e55c983ec6fb990da3b9854d2a1e
Lucas Vilas <93259672+lucasvxl@users.noreply.github.com>
2024-04-02 21:32:22 -0300

    Add files via upload

---
+++ Avaliacao-N1/avaliacao.c
@@ -0,0 +1,17 @@
+/*------------------------------------------------------------------------*/
+/*   FATEC-São Caetano do Sul                         Estrutura de Dados  */
+/*                         Rodrigo Martinez, Lucas                                 */
+/*             Objetivo: Avaliação Semana que vem                    */
+/*             Paradigama: Avalição                       */
+/*                                                                        */
+/*                                                       Data:02/04/2024  */
+/*------------------------------------------------------------------------*/ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Produto{
+int id;
+char nome[30];
+
+};
